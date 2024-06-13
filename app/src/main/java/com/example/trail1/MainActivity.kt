271be63974eb66053b8bf56295e4d56e536f5ae6
sathvik <sathvikgade@gmail.com>
2024-06-13 22:25:53 +0530

    button click log

--- app/src/main/java/com/example/trail1/MainActivity.kt
+++ app/src/main/java/com/example/trail1/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.trail1
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,9 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    fun myClickHandler(view: View) {
+        Log.i("MainActivity","Button clicked")
     }
 }
