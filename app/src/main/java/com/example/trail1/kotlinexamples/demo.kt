fd749238d473a689dfe3031cff805c8c75dadfac
sathvik <sathvikgade@gmail.com>
2024-06-13 14:33:02 +0530

    kotlin example

---
+++ app/src/main/java/com/example/trail1/kotlinexamples/demo.kt
@@ -0,0 +1,11 @@
+package com.example.trail1.kotlinexamples
+
+fun main() {
+    val numberOfFish = 50
+    val numberOfPlants = 23
+    if (numberOfFish > numberOfPlants) {
+        println("Good ratio!")
+    } else {
+        println("Unhealthy ratio")
+    }
+}
