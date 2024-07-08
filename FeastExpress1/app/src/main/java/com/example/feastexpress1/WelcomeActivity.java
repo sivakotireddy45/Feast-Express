9d87324501939386f55e2e2b573ee95633221751
sathvik <sathvikgade@gmail.com>
2024-07-08 13:05:31 +0530

    registration screen

--- FeastExpress1/app/src/main/java/com/example/feastexpress1/WelcomeActivity.java
+++ FeastExpress1/app/src/main/java/com/example/feastexpress1/WelcomeActivity.java
@@ -1,6 +1,8 @@
 package com.example.feastexpress1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,9 @@ public class WelcomeActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void register(View view) {
+        startActivity(new Intent(WelcomeActivity.this,RegistrationActivity.class));
     }
 }
